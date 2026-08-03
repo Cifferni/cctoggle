da477ca25d76b4326ae4ae9973066cf28237890f
cifferni <cifferni@163.com>
2026-08-04 01:38:34 +0800

    fix(ui): 用 @checked/@unchecked 事件替代 @update:value

--- src/components/common/TabBar.vue
+++ src/components/common/TabBar.vue
@@ -77,7 +77,7 @@ function onToggleProxy() {
       :title="proxyOn ? APP_LABELS[activeTab()] + ' 代理运行中，点击关闭' : '打开以为 ' + APP_LABELS[activeTab()] + ' 开启代理'"
     >
       <span class="proxy-label">代理</span>
-      <n-switch :value="proxyOn" size="small" tabindex="-1" @update:value="onToggleProxy" />
+      <n-switch :value="proxyOn" size="small" @checked="onToggleProxy" @unchecked="onToggleProxy" />
     </label>
 
     <button class="nav-btn" title="用量统计" @click="router.push('/stats')">
